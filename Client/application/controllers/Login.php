dbe4040d50bed88936029bf001eedeef187fec3a
Peni Sulistia - 20312066 <feniisulistia04@gmail.com>
2023-01-27 02:38:08 +0700

    membuat function rules di controller login di client

--- Client/application/controllers/Login.php
+++ Client/application/controllers/Login.php
@@ -2,6 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
+
+	public function _rules(){
+        $this->form_validation->set_rules('username', 'Username', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+    }
+
 	public function logout()
 	{
 		$this->session->sess_destroy();
