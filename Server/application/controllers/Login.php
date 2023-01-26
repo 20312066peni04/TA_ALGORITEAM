794301fe7d0e90ad381f4d1c0d5a1c6ce9848955
Alfi Baihaqi - 20312049 (KETUA) <alfi_baihaqi@teknokrat.ac.id>
2023-01-27 02:20:45 +0700

    membuat function service post di controller login di server

--- Server/application/controllers/Login.php
+++ Server/application/controllers/Login.php
@@ -11,4 +11,9 @@ class Login extends Server {
         parent::__construct();
         $this->load->model("ModelPengguna","model",TRUE);
     }
+
+    function service_post()
+    {
+        $this->response($this->model->get_login(array("username" => $this->post("username"), "password" => $this->post("password"))) ,200);
+    }
 }
