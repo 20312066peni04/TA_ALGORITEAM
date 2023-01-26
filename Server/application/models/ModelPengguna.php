aedbfe7512aa40a989751ed186b5f14bd297763c
Peni Sulistia - 20312066 <feniisulistia04@gmail.com>
2023-01-27 02:08:42 +0700

    membuat function get data di model pengguna

--- Server/application/models/ModelPengguna.php
+++ Server/application/models/ModelPengguna.php
@@ -2,6 +2,16 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class ModelPengguna extends CI_Model {
+
+    function get_data($data)
+    {
+      if (empty($data)){
+        $query = $this->db->query("SELECT * FROM pengguna WHERE pengguna.hapus = '1' ORDER BY pengguna.nama_pengguna ASC");
+      } else {
+        $query = $this->db->query("SELECT * FROM pengguna WHERE pengguna.hapus = '1' AND pengguna.id = '$data' ORDER BY pengguna.nama_pengguna ASC");
+      }
+      return $query->result();
+    }
     
 }
 
