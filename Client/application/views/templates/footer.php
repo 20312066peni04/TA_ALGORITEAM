d889da66433ffa260e24dc84396ac30ecd5d417e
Armelia Luvita Sari - 20312057 <armelia_luvita_sari@teknokrat.ac.id>
2023-01-27 02:32:24 +0700

    membuat template tampilan footer

---
+++ Client/application/views/templates/footer.php
@@ -0,0 +1,20 @@
+    <footer class="sticky-footer bg-white fixed-bottom">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Perpustakaan Teknokrat</span>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    
+    <script src="<?php echo base_url() ?>/ext/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url() ?>/ext/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo base_url() ?>/ext/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?php echo base_url() ?>/ext/js/sb-admin-2.min.js"></script>
+    <script src="<?php echo base_url() ?>/ext/vendor/chart.js/Chart.min.js"></script>
+    <script src="<?php echo base_url() ?>/ext/js/demo/chart-area-demo.js"></script>
+    <script src="<?php echo base_url() ?>/ext/js/demo/chart-pie-demo.js"></script>
+
+</body>
+</html>
