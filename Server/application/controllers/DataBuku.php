5652362ba59c67a179cd0db8bb1197dd9fd545cb
Armelia Luvita Sari - 20312057 <armelia_luvita_sari@teknokrat.ac.id>
2023-01-27 02:22:16 +0700

    membuat function service_post di controller data buku di server

--- Server/application/controllers/DataBuku.php
+++ Server/application/controllers/DataBuku.php
@@ -11,4 +11,15 @@ class DataBuku extends Server {
         parent::__construct();
         $this->load->model("ModelBuku","model",TRUE);
     }
+    
+    function service_post()
+    {
+        $data = array(
+            "nama_buku" => $this->post("nama_buku"),
+            "gambar" => $this->post("gambar"),
+            "hapus" => $this->post("hapus")
+        );
+
+        $this->response(array("status" => $this->model->save_data($data)),200);
+    }
 }
