c405de6f365dee0f685b94842b6fe4784cae5d8d
Peni Sulistia - 20312066 <feniisulistia04@gmail.com>
2023-01-27 02:07:58 +0700

    membuat function get data di model buku

--- Server/application/models/ModelBuku.php
+++ Server/application/models/ModelBuku.php
@@ -2,6 +2,13 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class ModelBuku extends CI_Model {
+
+    function get_data($data)
+    {
+      if (empty($data)) $query = $this->db->query("SELECT * FROM buku WHERE buku.hapus = '1' ORDER BY buku.nama_buku ASC");
+      else $query = $this->db->query("SELECT * FROM buku WHERE buku.hapus = '1' AND buku.id = '$data' ORDER BY buku.nama_buku ASC");
+      return $query->result();
+    }
     
 }
 
