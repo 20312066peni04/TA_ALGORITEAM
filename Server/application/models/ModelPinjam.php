cb4b486a4add967363fa9f5eec416c0ecc0b2ae8
Peni Sulistia - 20312066 <feniisulistia04@gmail.com>
2023-01-27 02:10:08 +0700

    membuat function get data di model pinjam

--- Server/application/models/ModelPinjam.php
+++ Server/application/models/ModelPinjam.php
@@ -2,6 +2,16 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class ModelPinjam extends CI_Model {
+
+    function get_data($data)
+    {
+      if (empty($data)){
+        $query = $this->db->query("SELECT * FROM pinjam WHERE pinjam.status = '1' ORDER BY pinjam.created ASC");
+      } else {
+        $query = $this->db->query("SELECT * FROM pinjam WHERE pinjam.id = '$data' AND pinjam.status = '1'");
+      }
+      return $query->result();
+    }
     
 }
 
