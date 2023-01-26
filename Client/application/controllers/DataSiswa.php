986e81db5587b32881d15227956a79a4c8c5cc43
Alfi Baihaqi - 20312049 (KETUA) <alfi_baihaqi@teknokrat.ac.id>
2023-01-27 02:41:38 +0700

    membuat function construct di controller data siswa di client

--- Client/application/controllers/DataSiswa.php
+++ Client/application/controllers/DataSiswa.php
@@ -2,6 +2,19 @@
 
 class DataSiswa extends CI_Controller{
     
+    public function __construct(){
+        parent::__construct();
+        if (empty($this->session->userdata('id'))){
+            $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>Anda belum login!</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>');
+			redirect("Login");
+        }
+    }
+    
 
     public function _rules(){
         $this->form_validation->set_rules('nama_lengkap', 'Nama Lengkap', 'required');
