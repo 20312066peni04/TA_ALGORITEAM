c1ec38c365b13075df55bf806cd3c4451bd32fc8
Armelia Luvita Sari - 20312057 <armelia_luvita_sari@teknokrat.ac.id>
2023-01-27 02:23:49 +0700

    membuat function service_post di controller data siswa di server

--- Server/application/controllers/DataSiswa.php
+++ Server/application/controllers/DataSiswa.php
@@ -11,4 +11,15 @@ class DataSiswa extends Server {
         parent::__construct();
         $this->load->model("ModelSiswa","model",TRUE);
     }
+    
+    function service_post()
+    {
+        $data = array(
+            "nis" => $this->input->post("nis"),
+            "nama_siswa" => $this->input->post("nama_siswa"),
+            "hapus" => $this->input->post("hapus")
+        );
+
+        $this->response(array("pesan" => $this->model->save_data($data)),200);
+    }
 }
