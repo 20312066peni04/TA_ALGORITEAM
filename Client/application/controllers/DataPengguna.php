5aeefa007796aea2ab8d84e459d3a8386f900eb5
Alfi Baihaqi - 20312049 (KETUA) <alfi_baihaqi@teknokrat.ac.id>
2023-01-27 02:40:16 +0700

    membuat function rules di controller data pengguna di client

--- Client/application/controllers/DataPengguna.php
+++ Client/application/controllers/DataPengguna.php
@@ -2,6 +2,13 @@
 
 class DataPengguna extends CI_Controller{
     
+
+    public function _rules(){
+        $this->form_validation->set_rules('nama_lengkap', 'Nama Lengkap', 'required');
+        $this->form_validation->set_rules('nip', 'NIP', 'required');
+        $this->form_validation->set_rules('username', 'Username', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+    }
 }
 
 ?>
