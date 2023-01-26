1b744e6c94667cb3898f9163f00c37c3d7dbd196
Peni Sulistia - 20312066 <feniisulistia04@gmail.com>
2023-01-27 02:11:18 +0700

    membuat function update data di model siswa

--- Server/application/models/ModelSiswa.php
+++ Server/application/models/ModelSiswa.php
@@ -12,6 +12,11 @@ class ModelSiswa extends CI_Model {
       }
       return $query->result();
     }
+
+    function save_data($data)
+    {
+      return $this->db->insert("siswa", $data);
+    }
     
 }
 
