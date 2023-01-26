67e2d453da06074910a3edcaa932253a2271daed
Alfi Baihaqi - 20312049 (KETUA) <alfi_baihaqi@teknokrat.ac.id>
2023-01-27 02:39:50 +0700

    membuat function rules di controller data siswa di client

--- Client/application/controllers/DataSiswa.php
+++ Client/application/controllers/DataSiswa.php
@@ -2,6 +2,11 @@
 
 class DataSiswa extends CI_Controller{
     
+
+    public function _rules(){
+        $this->form_validation->set_rules('nama_lengkap', 'Nama Lengkap', 'required');
+        $this->form_validation->set_rules('nis', 'NIS', 'required');
+    }
 }
 
 ?>
