b861bc412d62e523263ebcefd336829c6e2b5692
Peni Sulistia - 20312066 <feniisulistia04@gmail.com>
2023-01-27 02:37:24 +0700

    membuat tampilan update siswa

---
+++ Client/application/views/update_siswa.php
@@ -0,0 +1,28 @@
+<div class="container-fluid">
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800"><?php echo $title ?> </h1>
+
+    </div>
+
+    <div class="card">
+        <div class="card-body" style="width: 70%; margin-bottom: 80px">
+            <form action="<?= base_url('DataSiswa/update_data_aksi') ?>" method="post">
+            <input type="hidden" name="id" value="<?= $id ?>">
+                <div class="form-group">
+                    <label">Nama Lengkap</label>
+                    <input type="text" name="nama_lengkap" class="form-control" value="<?= $siswa[0]->nama_siswa ?>">
+                    <?= form_error('nama_lengkap', '<div class="text-small text-danger"></div>') ?>
+                </div>
+
+                <div class="form-group">
+                    <label">NIS</label>
+                    <input type="number" name="nis" class="form-control" value="<?= $siswa[0]->nis ?>">
+                    <?= form_error('nis', '<div class="text-small text-danger"></div>') ?>
+                </div>
+
+                <button type="submit" class="btn btn-success">SIMPAN</button>
+            </form>
+        </div>
+    </div>
+
+</div>
