1cf08ac454659ea68d02fc8fc7283926d0164239
Peni Sulistia - 20312066 <feniisulistia04@gmail.com>
2023-01-27 02:37:54 +0700

    membuat function logout di controller login di client

--- Client/application/controllers/Login.php
+++ Client/application/controllers/Login.php
@@ -2,4 +2,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
+	public function logout()
+	{
+		$this->session->sess_destroy();
+		redirect('Login');
+	}
 }
