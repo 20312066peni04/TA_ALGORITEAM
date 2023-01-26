e39fa1e8b0616b64c37407896ec64b3f8e10bae6
Alfi Baihaqi - 20312049 (KETUA) <alfi_baihaqi@teknokrat.ac.id>
2023-01-27 02:17:13 +0700

    membuat function construck di controller data buku di server

--- Server/application/controllers/DataBuku.php
+++ Server/application/controllers/DataBuku.php
@@ -5,4 +5,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH."libraries/Server.php";
 
 class DataBuku extends Server {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("ModelBuku","model",TRUE);
+    }
 }
