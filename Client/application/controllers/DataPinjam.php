1fb903a90e4ba50cdc1602c8516ef47ca4db85ca
Alfi Baihaqi - 20312049 (KETUA) <alfi_baihaqi@teknokrat.ac.id>
2023-01-27 02:40:03 +0700

    membuat function rules di controller data pinjam di client

--- Client/application/controllers/DataPinjam.php
+++ Client/application/controllers/DataPinjam.php
@@ -2,6 +2,11 @@
 
 class DataPinjam extends CI_Controller{
     
+
+    public function _rules(){
+        $this->form_validation->set_rules('id_buku', 'ID Buku', 'required');
+        $this->form_validation->set_rules('id_siswa', 'ID Siswa/Siswi', 'required');
+    }
 }
 
 ?>
