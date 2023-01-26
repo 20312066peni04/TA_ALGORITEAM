b3f532f568e30c25f586667ea3a03356549c12d8
Armelia Luvita Sari - 20312057 <armelia_luvita_sari@teknokrat.ac.id>
2023-01-27 02:22:51 +0700

    membuat function service_post di controller data pengguna update di server

--- Server/application/controllers/DataPenggunaUpdate.php
+++ Server/application/controllers/DataPenggunaUpdate.php
@@ -12,4 +12,16 @@ class DataPenggunaUpdate extends Server {
         $this->load->model("ModelPengguna","model",TRUE);
     }
     
+    function service_post()
+    {
+        $data = array(
+            "nip" => $this->input->post("nip"),
+            "nama_pengguna" => $this->input->post("nama_pengguna"),
+            "username" => $this->input->post("username"),
+            "password" => $this->input->post("password"),
+            "hapus" => $this->input->post("hapus")
+        );
+        
+        $this->response(array("pesan" => $this->model->update_data($data, $this->input->post("id"))),200);
+    }
 }
