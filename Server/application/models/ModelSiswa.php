21bb55366ca7c8f612af04975e149fe927b2c457
Peni Sulistia - 20312066 <feniisulistia04@gmail.com>
2023-01-27 02:11:03 +0700

    membuat function get data di model siswa

--- Server/application/models/ModelSiswa.php
+++ Server/application/models/ModelSiswa.php
@@ -2,6 +2,16 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class ModelSiswa extends CI_Model {
+
+    function get_data($data)
+    {
+      if (empty($data)){
+        $query = $this->db->query("SELECT * FROM siswa WHERE siswa.hapus = '1' ORDER BY siswa.nama_siswa ASC");
+      } else {
+        $query = $this->db->query("SELECT * FROM siswa WHERE siswa.hapus = '1' AND siswa.id = '$data' ORDER BY siswa.nama_siswa ASC");
+      }
+      return $query->result();
+    }
     
 }
 
