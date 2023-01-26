d82cf5787f7bf977f84189e12f207ca60c9ea180
Peni Sulistia - 20312066 <feniisulistia04@gmail.com>
2023-01-27 02:37:07 +0700

    membuat tampilan update buku

---
+++ Client/application/views/update_buku.php
@@ -0,0 +1,28 @@
+<div class="container-fluid">
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800"><?php echo $title ?> </h1>
+
+    </div>
+
+    <div class="card">
+        <div class="card-body" style="width: 70%; margin-bottom: 80px">
+            <form action="<?= base_url('DataBuku/update_data_aksi/') ?>" method="post" enctype="multipart/form-data">
+                <input type="hidden" name="id" value="<?= $id ?>">
+                <div class="form-group">
+                    <label">Nama Buku</label>
+                    <input type="text" name="nama_buku" class="form-control" value="<?= $buku[0]->nama_buku ?>">
+                    <?= form_error('nama_buku', '<div class="text-small text-danger"></div>') ?>
+                </div>
+
+                <div class="form-group">
+                    <label">Gambar</label>
+                    <input type="file" name="photo" class="form-control">
+                    <?= form_error('photo', '<div class="text-small text-danger"></div>') ?>
+                </div>
+
+                <button type="submit" class="btn btn-success">UPDATE</button>
+            </form>
+        </div>
+    </div>
+
+</div>
