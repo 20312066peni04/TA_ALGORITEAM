76e0c923eb4f9d12e263062f92ee89b4230fbe9a
Armelia Luvita Sari - 20312057 <armelia_luvita_sari@teknokrat.ac.id>
2023-01-27 02:46:55 +0700

    membuat function delete data di controller data buku di client

--- Client/application/controllers/DataBuku.php
+++ Client/application/controllers/DataBuku.php
@@ -37,6 +37,40 @@ class DataBuku extends CI_Controller{
         $this->load->view('tambah_buku',$data);
         $this->load->view('templates/footer');
     }
+
+    public function delete_data($id)
+    {
+        $send = array('id' => $id);
+        $response = json_decode($this->client->simple_get(API_BUKU, $send));
+        $data['buku'] = $response->buku;
+
+        $data = array(
+            'id' => $id,
+            'nama_buku' => $response->buku[0]->nama_buku,
+            'gambar' => $response->buku[0]->gambar,
+            'hapus' => '0'
+        );
+
+        $response = json_decode($this->client->simple_post(API_BUKU . 'Update', $data));
+        if ($response->pesan){
+            $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Data berhasil dihapus</strong> 
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>');
+        } else {
+            $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Data gagal dihapus</strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>');
+        }
+
+        $this->index();
+    }
+
     public function _rules(){
         $this->form_validation->set_rules('nama_buku', 'Nama Buku', 'required');
     }
