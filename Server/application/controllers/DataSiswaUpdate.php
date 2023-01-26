0ea7ecb8fb3f390eec82d0763f8360fec305b01e
Alfi Baihaqi - 20312049 (KETUA) <alfi_baihaqi@teknokrat.ac.id>
2023-01-27 02:19:15 +0700

    membuat function construct di controller data siswa update di server

--- Server/application/controllers/DataSiswaUpdate.php
+++ Server/application/controllers/DataSiswaUpdate.php
@@ -5,4 +5,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH."libraries/Server.php";
 
 class DataSiswaUpdate extends Server {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("ModelSiswa","model",TRUE);
+    }
+    
 }
