4776fa024ed0ed51a414702b562dce12335041b9
Armelia Luvita Sari - 20312057 <armelia_luvita_sari@teknokrat.ac.id>
2023-01-27 16:35:37 +0700

    membuat function tambah data aksi di controller data pinjam di client

--- Client/application/controllers/DataPinjam.php
+++ Client/application/controllers/DataPinjam.php
@@ -35,6 +35,41 @@ class DataPinjam extends CI_Controller{
         $this->load->view('tambah_peminjam',$data);
         $this->load->view('templates/footer');
     }
+
+    public function tambah_data_aksi()
+    {
+        $this->_rules();
+
+        if ($this->form_validation->run() == FALSE){
+            $this->tambah_data();
+        } else {
+            $data = array(
+                'id_siswa' => $this->input->post('id_siswa'),
+                'id_buku' => $this->input->post('id_buku'),
+                'id_pengguna' => $this->session->userdata('id'),
+                'status' => "1"
+            );
+
+            $response = json_decode($this->client->simple_post(API_PINJAM, $data));
+            if ($response->pesan){
+                $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong>Data berhasil ditambahkan</strong> 
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>');
+            } else {
+                $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>Data gagal ditambahkan</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>');
+            }
+
+            $this->index();
+        }
+    }
     public function _rules(){
         $this->form_validation->set_rules('id_buku', 'ID Buku', 'required');
         $this->form_validation->set_rules('id_siswa', 'ID Siswa/Siswi', 'required');
