89f33ee25a605b71de20e5f92266ba6dab7fb82b
Armelia Luvita Sari - 20312057 <armelia_luvita_sari@teknokrat.ac.id>
2023-01-27 16:38:02 +0700

    membuat function update data aksi di controller data siswa di client

--- Client/application/controllers/DataSiswa.php
+++ Client/application/controllers/DataSiswa.php
@@ -76,6 +76,34 @@ class DataSiswa extends CI_Controller{
         $this->load->view('update_siswa',$data);
         $this->load->view('templates/footer');
     }
+
+    public function update_data_aksi()
+    {
+        $this->_rules();
+        if ($this->form_validation->run() == FALSE){
+            $this->index();
+        } else {
+            $response = json_decode($this->client->simple_post(API_SISWA . 'Update', array('id' => $this->input->post('id'), 'nis' => $this->input->post('nis'), 'nama_siswa' => $this->input->post('nama_lengkap'), 'hapus'=>'1')));
+            if ($response->pesan){
+                $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong>Data berhasil diUpdate</strong> 
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>');
+            } else {
+                $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>Data gagal diUpdate</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                </div>');
+            }
+
+            $this->index();
+        }
+    }
+
     public function delete_data($id)
     {
         $send = array('id' => $id);
