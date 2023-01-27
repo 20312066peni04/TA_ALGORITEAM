68334b02e7ff47cdbfecff1d764e687f06b242f2
Armelia Luvita Sari - 20312057 <armelia_luvita_sari@teknokrat.ac.id>
2023-01-27 16:37:26 +0700

    membuat function update data di controller data pinjam di client

--- Client/application/controllers/DataPinjam.php
+++ Client/application/controllers/DataPinjam.php
@@ -70,6 +70,41 @@ class DataPinjam extends CI_Controller{
             $this->index();
         }
     }
+
+    public function update_data($id)
+    {
+        $send = array('id' => $id);
+        $response = json_decode($this->client->simple_get(API_PINJAM, $send));
+
+        $data = array(
+            'id' => $id,
+            'id_siswa' => $response->pinjam[0]->id_siswa,
+            'id_buku' => $response->pinjam[0]->id_buku,
+            'id_pengguna' => $this->session->userdata('id'),
+            'status' => "0"
+        );
+
+        $response = json_decode($this->client->simple_post(API_PINJAM . 'Update', $data));
+        if ($response->pesan){
+            $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Data berhasil disimpan</strong> 
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>');
+        } else {
+            $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Data gagal disimpan</strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>');
+        }
+
+
+        $this->index();
+    }
+
     public function _rules(){
         $this->form_validation->set_rules('id_buku', 'ID Buku', 'required');
         $this->form_validation->set_rules('id_siswa', 'ID Siswa/Siswi', 'required');
